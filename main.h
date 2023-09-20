43f7847439bc3ee31607ca322d8adf09fb62b7ff
Maboakye <aduseiboakyem@gmail.com>
2023-09-19 17:00:56 -0700

    task 1

--- main.h
+++ main.h
@@ -7,6 +7,23 @@
 int _printf(const char *format, ...);
 int binary(int num);
 int octal(int num);
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int sizeo);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+
+
+
 
 
 #endif
